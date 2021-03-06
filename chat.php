a08150266e0e39aa4d14a78d97967b14fa9c2634
emetdas <emet@b2bleadstaff.com>
2021-03-06 21:10:39 +0600

    just poire hori hia

--- chat.php
+++ chat.php
@@ -1,112 +1,93 @@
-<?php include 'header.php';?>
-  <body>
-    <div class="wraper">
-      <section class="chat-area">
-        <header>
-          <a href="#" class="back-icon">
-            <i class="fas fa-arrow-left"></i>
-          </a>
-          <img
-            src="assets/images/myimage.png"
-            alt="profile Image"
-            class="profile-img"
-          />
-          <div class="details">
-            <span>Emet Das</span>
-            <p>Active now</p>
-          </div>
-        </header>
-        <div class="chat-box">
-          <div class="chat outgoing">
-            <div class="deatils">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Commodi, debitis!
-              </p>
-            </div>
-          </div>
-          <div class="chat icoming">
-            <img
-              src="assets/images/myimage.png"
-              alt="Profile image"
-              class="profile-img"
-            />
-            <div class="deatils">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Commodi, debitis!
-              </p>
-            </div>
-          </div>
-          <div class="chat outgoing">
-            <div class="deatils">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Commodi, debitis!
-              </p>
-            </div>
-          </div>
-          <div class="chat icoming">
-            <img
-              src="assets/images/myimage.png"
-              alt="Profile image"
-              class="profile-img"
-            />
-            <div class="deatils">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Commodi, debitis!
-              </p>
-            </div>
-          </div>
-          <div class="chat outgoing">
-            <div class="deatils">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Commodi, debitis!
-              </p>
-            </div>
-          </div>
-          <div class="chat icoming">
-            <img
-              src="assets/images/myimage.png"
-              alt="Profile image"
-              class="profile-img"
-            />
-            <div class="deatils">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Commodi, debitis!
-              </p>
-            </div>
-          </div>
-          <div class="chat outgoing">
-            <div class="deatils">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Commodi, debitis!
-              </p>
-            </div>
-          </div>
-          <div class="chat icoming">
-            <img
-              src="assets/images/myimage.png"
-              alt="Profile image"
-              class="profile-img"
-            />
-            <div class="deatils">
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Commodi, debitis!
-              </p>
-            </div>
+<?php include 'header.php'; ?>
+<body>
+  <div class="wraper">
+    <section class="chat-area">
+      <header>
+        <a href="user.php" class="back-icon">
+          <i class="fas fa-arrow-left"></i>
+        </a>
+        <img src="assets/images/myimage.png" alt="profile Image" class="profile-img" />
+        <div class="details">
+          <span>Emet Das</span>
+          <p>Active now</p>
+        </div>
+      </header>
+      <div class="chat-box">
+        <div class="chat outgoing">
+          <div class="deatils">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Commodi, debitis!
+            </p>
           </div>
         </div>
-        <form action="#" class="typing-area">
-          <input type="text" placeholder="Type the massege here..">
-          <button><i class="fab fa-telegram-plane"></i></button>
-        </form>
-      </section>
-    </div>
-  </body>
+        <div class="chat icoming">
+          <img src="assets/images/myimage.png" alt="Profile image" class="profile-img" />
+          <div class="deatils">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Commodi, debitis!
+            </p>
+          </div>
+        </div>
+        <div class="chat outgoing">
+          <div class="deatils">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Commodi, debitis!
+            </p>
+          </div>
+        </div>
+        <div class="chat icoming">
+          <img src="assets/images/myimage.png" alt="Profile image" class="profile-img" />
+          <div class="deatils">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Commodi, debitis!
+            </p>
+          </div>
+        </div>
+        <div class="chat outgoing">
+          <div class="deatils">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Commodi, debitis!
+            </p>
+          </div>
+        </div>
+        <div class="chat icoming">
+          <img src="assets/images/myimage.png" alt="Profile image" class="profile-img" />
+          <div class="deatils">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Commodi, debitis!
+            </p>
+          </div>
+        </div>
+        <div class="chat outgoing">
+          <div class="deatils">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Commodi, debitis!
+            </p>
+          </div>
+        </div>
+        <div class="chat icoming">
+          <img src="assets/images/myimage.png" alt="Profile image" class="profile-img" />
+          <div class="deatils">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Commodi, debitis!
+            </p>
+          </div>
+        </div>
+      </div>
+      <form action="#" class="typing-area">
+        <input type="text" placeholder="Type the massege here..">
+        <button><i class="fab fa-telegram-plane"></i></button>
+      </form>
+    </section>
+  </div>
+</body>
+
 </html>
