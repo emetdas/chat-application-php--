70db1df90c87b4cb6b028a6fdc9b13a4b05013da
emetdas <emet@b2bleadstaff.com>
2021-06-21 15:35:25 +0600

    all file is deleted

--- header.php
+++ header.php
@@ -1,48 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="keywords" content="Top Chat for Emet,chat application" />
-    <meta name="description" content="chat app develop my Emet Das in php." />
-    <meta name="author" content="Emet Das" />
-    <!-- facebook and twitter seo -->
-    <meta property="og:title" content="Chat Application Develop by Emet" />
-    <meta
-      property="og:description"
-      content="Best Chat application Develop by Emet Das.using Php and Font-end framework"
-    />
-    <meta property="og:image" content="assets/images/illustation-for-og.jpg" />
-    <meta property="og:url" content="index.htm" />
-    <!-- twitter -->
-    <meta property="og:site_name" content="Chat Application By Emet" />
-    <meta name="twitter:title" content="Chat Application Develop by Emet" />
-    <meta
-      name="twitter:description"
-      content="Best Chat application Develop by Emet Das.using Php and Font-end framework"
-    />
-    <meta name="twitter:image" content="assets/images/illustation-for-og.jpg" />
-    <meta name="twitter:image:alt" content="Chat Application" />
-    <meta name="twitter:card" content="summary" />
-    <meta name="twitter:site" content="@emet" />
-    <!-- facebook and twitter seo -->
-    <link
-      rel="shortcut icon"
-      href="assets/images/chat.svg"
-      type="image/x-icon"
-    />
-    <link rel="apple-touch-icon" href="assets/images/chat.svg" />
-    <title>Chat For Emet</title>
-    <!-- google-fonts -->
-    <link rel="preconnect" href="https://fonts.gstatic.com" />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
-      rel="stylesheet"
-    />
-    <!-- google-fonts -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- css file -->
-    <link rel="stylesheet" href="css/style.css" />
-    <!-- css file -->
-  </head>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="keywords" content="Top Chat for Emet,chat application" />
+  <meta name="description" content="chat app develop my Emet Das in php." />
+  <meta name="author" content="Emet Das" />
+  <!-- facebook and twitter seo -->
+  <meta property="og:title" content="Chat Application Develop by Emet" />
+  <meta property="og:description" content="Best Chat application Develop by Emet Das.using Php and Font-end framework" />
+  <meta property="og:image" content="assets/images/illustation-for-og.jpg" />
+  <meta property="og:url" content="index.htm" />
+  <!-- twitter -->
+  <meta property="og:site_name" content="Chat Application By Emet" />
+  <meta name="twitter:title" content="Chat Application Develop by Emet" />
+  <meta name="twitter:description" content="Best Chat application Develop by Emet Das.using Php and Font-end framework" />
+  <meta name="twitter:image" content="assets/images/illustation-for-og.jpg" />
+  <meta name="twitter:image:alt" content="Chat Application" />
+  <meta name="twitter:card" content="summary" />
+  <meta name="twitter:site" content="@emet" />
+  <!-- facebook and twitter seo -->
+  <link rel="shortcut icon" href="assets/images/chat.svg" type="image/x-icon" />
+  <link rel="apple-touch-icon" href="assets/images/chat.svg" />
+  <title>Chat For Emet</title>
+  <!-- google-fonts -->
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+  <!-- google-fonts -->
+  <link rel="stylesheet" href="node_modules/@fortawesome/fontawesome-free/css/all.min.css">
+  <!-- css file -->
+  <link rel="stylesheet" href="css/style.css" />
+  <!-- css file -->
+</head>
