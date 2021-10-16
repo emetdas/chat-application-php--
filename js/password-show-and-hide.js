d47c4bfc78c1e395d1fde856d9af09e443cb62ce
emetdas <emet@b2bleadstaff.com>
2021-10-16 16:56:12 +0600

    update is done

--- js/password-show-and-hide.js
+++ js/password-show-and-hide.js
@@ -1,13 +1,13 @@
 const passwordInput = document.getElementById('password');
-const showIcon = document.querySelector('.fa-key');
-showIcon.addEventListener('click',()=>{
-    if (passwordInput.type === "password") {
-        passwordInput.type = "text";
-        showIcon.classList.add('fa-eye-slash');
-        showIcon.classList.remove('fa-key');
-      } else {
-        passwordInput.type = "password";
-        showIcon.classList.add('fa-key');
-        showIcon.classList.remove('fa-eye-slash');
-      }
-})
+const showIcon = document.querySelector('.fa-eye');
+showIcon.addEventListener('click', () => {
+  if (passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+    showIcon.classList.add('fa-eye-slash');
+    showIcon.classList.remove('fa-eye');
+  } else {
+    passwordInput.type = 'password';
+    showIcon.classList.add('fa-eye');
+    showIcon.classList.remove('fa-eye-slash');
+  }
+});
